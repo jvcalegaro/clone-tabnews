4acc7a27159eb7285006f863af76eaa9d5c6eb21
Vitor Calegaro <75556101+jvcalegaro@users.noreply.github.com>
2023-09-07 23:02:37 +0000

    Frases...

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ function Home() {
     <h3>"O homem tem duas vidas: a segunda começa quando ele percebe que tem apenas uma." - Confúcio</h3>,
     <h3>"A vida é o que acontece enquanto você está ocupado fazendo outros planos." - John Lennon</h3>,
     <h3>"O ontem é história, o amanhã é um mistério, mas o hoje é uma dádiva. É por isso que se chama presente." - Kung Fu Panda</h3>,
-    <h3> </h3>,
     <h3>Se não houver amanhã, você foi feliz hoje?</h3>]
     return frase
 }
