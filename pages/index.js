95d5b83a6e56089245e3debd65c90839e04fe124
Vitor Calegaro <75556101+jvcalegaro@users.noreply.github.com>
2023-09-07 20:53:15 +0000

    organizando o repositório

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Teste 3</h1>
+    const frase = [<h1>Pense bem...</h1>,
+    <h3>"O homem tem duas vidas: a segunda começa quando ele percebe que tem apenas uma." - Confúcio</h3>,
+    <h3>"A vida é o que acontece enquanto você está ocupado fazendo outros planos." - John Lennon</h3>,
+    <h3>"O ontem é história, o amanhã é um mistério, mas o hoje é uma dádiva. É por isso que se chama presente." - Kung Fu Panda</h3>,
+    <h3> </h3>,
+    <h3>Se não houver amanhã, você foi feliz hoje?</h3>]
+    return frase
 }
 
 export default Home;
