8fff7b17d747c08d2e8fb1b47cd65f8d2d5bec39
Vitor Calegaro <75556101+jvcalegaro@users.noreply.github.com>
2023-10-06 19:31:56 +0000

    frase

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
 function Home() {
-    return (
-        <div style={{ backgroundColor: 'black' }}>
-            <iframe
-                id="meuIframe"
-                width="853"
-                height="480"
-                src="https://www.youtube.com/embed/u9EgZsBWDF0"
-                title="Sorria"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
-                style={{ backgroundColor: 'black' }}
-            ></iframe>
-        </div>
-    );
+    const frase = [<h1>Que raça de cachorro pula mais alto que um prédio?</h1>,
+    <h2> </h2>,
+    <h3>"Qualquer uma, porque prédio não pula.</h3>]
+    return frase
 }
-
 export default Home;
