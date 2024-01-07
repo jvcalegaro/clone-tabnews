efb16af3caa5c176ab3f02bf753e3de726d12f90
Vitor Calegaro <75556101+jvcalegaro@users.noreply.github.com>
2024-01-07 12:14:39 +0000

    adiciona scripts ´lint:check´ e ´lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
-    const frase = [<h1>Que raça de cachorro pula mais alto que um prédio?</h1>,
+  const frase = [
+    <h1>Que raça de cachorro pula mais alto que um prédio?</h1>,
     <h2> </h2>,
-    <h3>"Qualquer uma, porque prédio não pula.</h3>]
-    return frase
+    <h3>"Qualquer uma, porque prédio não pula.</h3>,
+  ];
+  return frase;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 export default Home;
